1bafafb02ea617acd37ac323aab0957adbaaacf6
ZeynepTuana06 <168047290+ZeynepTuana06@users.noreply.github.com>
2024-05-20 18:24:43 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main() {
+
+  String firstName = "Zeynep";
+  String LastName= "Ardanuç";
+  int yas = 15;
+  bool resitlik_durum = false;
+
+  print("Ben $firstName $LastName");
+  print("Yaşım $yas");
+  print("Reşitlik Durumum: $resitlik_durum");
+}
